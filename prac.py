583bf58c80b593f70c4c7475c23a03005a039a99
DSondhi <dhwanisondhi3@gmail.com>
2020-02-23 09:20:33 -0500

    Changed some errors

--- prac.py
+++ prac.py
@@ -6,8 +6,8 @@ from rdflib.namespace import DC, FOAF
 g = rdflib.Graph()
 g.parse("new.ttl", format="ttl")
 
-bob = URIRef("http://example.org/people/Bob")
-g.add((bob, FOAF.Name, FOAF.Literal))
+#bob = URIRef("http://example.org/people/Bob")
+#g.add((bob, FOAF.Name, FOAF.Literal))
 
 for subj, pred, obj in g:
 	print(subj, pred, obj)
